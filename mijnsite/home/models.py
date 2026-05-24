1816361a2ced9572f121a2cfa47faf7f2446167d
glorysay96-aw <glory.say.96@gmail.com>
2026-05-24 19:44:35 +0200

    contact

--- mijnsite/home/models.py
+++ mijnsite/home/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
 from wagtail.models import Page
+from wagtail.snippets.models import register_snippet
 
 
 class HomePage(Page):
@@ -26,3 +27,16 @@ class ErvaringenPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel("tekst"),
     ]
+
+@register_snippet
+class ContactInfo(models.Model):
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+
+    panels = [
+        FieldPanel("email"),
+        FieldPanel("phone_number"),
+    ]
+
+    def __str__(self):
+        return "Contact informatie"   
