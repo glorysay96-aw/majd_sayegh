0179ccc8bc700d8f938bd472c1851fe9dd55d94a
glorysay96-aw <glory.say.96@gmail.com>
2026-05-24 15:17:05 +0200

    hero

--- mijnsite/home/models.py
+++ mijnsite/home/models.py
@@ -1,7 +1,21 @@
 from django.db import models
-
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
 from wagtail.models import Page
 
 
 class HomePage(Page):
-    pass
+    intro = RichTextField(blank=True)
+
+    hero_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("intro"),
+        FieldPanel("hero_image"),
+    ]
